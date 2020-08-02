892ff83a66a3e184decbc5f9ecadbb539c8e4d75
Reshal Luchman (ZA) <Reshal.Luchman@absa.africa>
2020-08-02 13:55:17 +0200

    Added view bicycles journey to test.

---
+++ velo/velo/Journey/View Bicycles Journey/View Model/ViewBicyclesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewBicyclesViewModel.swift
+//  velo
+//
+//  Created by Reshal Luchman on 2020/08/02.
+//  Copyright © 2020 ReshalLuchman. All rights reserved.
+//
+
+import Foundation
+
+struct ViewBicyclesViewModel {
+}
---
+++ velo/velo/Journey/View Bicycles Journey/ViewBicyclesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewBicyclesViewController.swift
+//  velo
+//
+//  Created by Reshal Luchman on 2020/08/02.
+//  Copyright © 2020 ReshalLuchman. All rights reserved.
+//
+
+import UIKit
+
+class ViewBicyclesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
