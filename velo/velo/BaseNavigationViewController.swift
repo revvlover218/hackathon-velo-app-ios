2ae466025df5f521ad496abfa49d365f77d14d07
Reshal Luchman (ZA) <Reshal.Luchman@absa.africa>
2020-08-02 19:15:47 +0200

    Added some navigation logic.

---
+++ velo/velo/BaseNavigationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseNavigationViewController.swift
+//  velo
+//
+//  Created by Reshal Luchman on 2020/08/02.
+//  Copyright © 2020 ReshalLuchman. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigation()
+    }
+    
+    private func configureNavigation() {
+    }
+}
