1834eb4b4462e00112d7d0c17bfa63666509d83d
Reshal Luchman (ZA) <Reshal.Luchman@absa.africa>
2020-08-06 19:55:43 +0200

    Added search screen.

---
+++ velo/velo/SearchBicyclesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchBicyclesViewController.swift
+//  velo
+//
+//  Created by Reshal Luchman on 2020/08/06.
+//  Copyright © 2020 ReshalLuchman. All rights reserved.
+//
+
+import UIKit
+
+class SearchBicyclesViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
