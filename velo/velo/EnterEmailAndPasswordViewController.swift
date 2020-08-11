e2a50f8cc0bf0c39c1074d08d7a73e9936d2dfa4
Reshal Luchman (ZA) <Reshal.Luchman@absa.africa>
2020-08-11 08:57:49 +0200

    Added scrolling and keyboard responder methods etc.

--- velo/velo/EnterEmailAndPasswordViewController.swift
+++ velo/velo/EnterEmailAndPasswordViewController.swift
@@ -2,7 +2,7 @@
 //  EnterEmailAndPasswordViewController.swift
 //  velo
 //
-//  Created by ReshalAdmin on 2020/08/11.
+//  Created by Reshal Luchman on 2020/08/11.
 //  Copyright © 2020 ReshalLuchman. All rights reserved.
 //
 
@@ -12,7 +12,8 @@ class EnterEmailAndPasswordViewController: BaseViewController {
 
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var logoView: CustomUIView!
-    
+    @IBOutlet var emailInputView: CustomTextField!
+    @IBOutlet var passwordInputView: CustomTextField!
     @IBOutlet var forgotPasswordButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     
@@ -24,11 +25,48 @@ class EnterEmailAndPasswordViewController: BaseViewController {
         configureUI()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if !(scrollView.contentOffset.y == -100) {
+            scrollView.contentOffset.y = -250
+        }
+    }
+    
+    // MARK: - IBOutlet
+    
+    @IBAction private func scrollUp() {
+        UIView.animate(withDuration: 0.2) {
+            self.scrollView.contentOffset.y = -100
+        }
+    }
+    
+    @IBAction private func scrollDown() {
+        UIView.animate(withDuration: 0.2) {
+            self.scrollView.contentOffset.y = -250
+        }
+    }
+    
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        let alertView = UIAlertController(title: "Velo",
+                                          message: "This feature will be availabe soon! Thank you.",
+                                          preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Dismiss", style: .default)
+        alertView.addAction(alertAction)
+        self.present(alertView, animated: true, completion: nil)
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: RoundedSquareButton) {
+        // Goto --> 4 main buttons screen
+    }
     
     // MARK: - Private
     
     private func configureUI() {
-        scrollView.contentOffset.y = 200
         configureButtons()
     }
     
@@ -45,9 +83,20 @@ class EnterEmailAndPasswordViewController: BaseViewController {
 
 extension EnterEmailAndPasswordViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.resignFirstResponder()
+        switch textField {
+        case emailInputView:
+            if let passwordInputView = passwordInputView {
+                passwordInputView.becomeFirstResponder()
+            }
+        case passwordInputView:
+            passwordInputView.resignFirstResponder()
+        default:
+            break
+        }
         return true
     }
+    
+    
 }
 
 extension EnterEmailAndPasswordViewController: UIScrollViewDelegate {
